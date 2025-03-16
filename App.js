0143095a95b7d231dbb494fa071bd262f87d7184
White-Wolf-18 <mithileshreddybavusani@gmail.com>
2025-03-16 11:08:08 +0530

    Added Login and Signup page

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StudentLandingPage from "./Pages/StudentLandingPage";
+import StaffLandingPage from "./Pages/StaffLandingPage";
+import StudentProfile from "./Pages/StudentProfile";
+import StaffProfile from "./Pages/StaffProfile";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/studentprofile" element={<StudentProfile />} />
+        <Route path="/staffprofile" element={<StaffProfile />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Pages/StaffProfile.js
@@ -0,0 +1,59 @@
+import "../Styles/StaffProfile.css";
+import React from "react";
+
+const StaffProfile = () => {
+  return (
+    <div className="container">
+      {/* Header Section */}
+      <header className="header">
+        <div className="top-section">
+          <div className="profile-circle">A</div>
+          <h1 className="title">DELIVERY MANAGEMENT</h1>
+          <div className="Logout-symbol"></div>
+        </div>
+        <nav className="nav-bar">
+          <ul className="nav-links">
+            <li><a href="/">HOME</a></li>
+            <li><a href="#">PARCEL STATUS</a></li>
+            <li><a href="#">COMPLAINTS</a></li>
+            <li><a href="#">FEEDBACK</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      {/* Main Content Section */}
+      <main className="content">
+        <h2 className="profile-title"><i>My Profile</i></h2>
+        <div className="profile-section">
+          <table>
+            <tbody>
+              <tr>
+                <td><strong>Name</strong></td>
+                <td>Alice</td>
+              </tr>
+              <tr>
+                <td><strong>NITC Email Address</strong></td>
+                <td>alice@nitc.ac.in</td>
+              </tr>
+              <tr>
+                <td><strong>Staff ID</strong></td>
+                <td>XXXXXXXX</td>
+              </tr>
+              <tr>
+                <td><strong>Gender</strong></td>
+                <td>Female</td>
+              </tr>
+              <tr>
+                <td><strong>Mobile Number</strong></td>
+                <td>9876543210</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <button className="edit-button">Edit details</button>
+      </main>
+    </div>
+  );
+};
+
+export default StaffProfile;
